90e9daae68a691c0256d9ebc2bb720822ea0aecb
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2020-08-24 22:58:08 +0700

    Implements folding on defmodule
    Work only on file that has only 1 defmodule. Otherwise, not work.

--- apps/language_server/lib/language_server/providers/folding.ex
+++ apps/language_server/lib/language_server/providers/folding.ex
@@ -2,13 +2,34 @@ defmodule ElixirLS.LanguageServer.Providers.Folding do
   def folding_ranges(source_file) do
     ranges =
       source_file.text
-      |> Code.string_to_quoted!(columns: true)
-      |> folding_range_do_block()
+      |> Code.string_to_quoted!()
+      |> IO.inspect()
+      |> do_block_folding_range()
 
     {:ok, ranges}
   end
 
-  defp folding_range_do_block(_ast) do
-    []
+  defp do_block_folding_range({:defmodule, [line: line], [_module, doblock]}) do
+    case doblock do
+      [do: {:__block__, [], body}] ->
+        [
+          %{
+            "startLine" => line - 1,
+            "endLine" => line - 1 + counting_line(body)
+          }
+        ]
+
+      _ ->
+        []
+    end
+  end
+
+  defp counting_line(statements) do
+    {:def, [line: line], [_, body]} =
+      statements
+      |> Enum.reverse()
+      |> hd()
+
+    line + Enum.count(body)
   end
 end
