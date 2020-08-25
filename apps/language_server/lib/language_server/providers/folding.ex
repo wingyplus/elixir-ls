2d9a94ceb22147ab9c2e128cf029519f8bc0c758
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2020-08-25 23:23:13 +0700

    Re-implement using :elixir_tokenizer
    :elixir_tokenizer will produces tokennizes that mark start and end do
    block. Using it is incorrect than using Code.string_to_quoted/1 and work
    perfectly in case nest do/end block such as function, case block, etc.
    
    This changes have some bugs needs to solve. Such as: handling
    un-balanced do/end block (oneline do block) or multi defmodule in
    top-level file.

--- apps/language_server/lib/language_server/providers/folding.ex
+++ apps/language_server/lib/language_server/providers/folding.ex
@@ -20,69 +20,63 @@ defmodule ElixirLS.LanguageServer.Providers.Folding do
   def folding_ranges(source_file) do
     ranges =
       source_file.text
-      |> Code.string_to_quoted!(line: 0)
-      |> IO.inspect()
-      |> do_block_folding_range()
+      |> String.to_charlist()
+      |> :elixir_tokenizer.tokenize(0, [])
+      |> case do
+        {:ok, tokens} ->
+          tokens
+          |> do_block_folding_range()
+
+        {:error, _} ->
+          []
+      end
 
     {:ok, ranges}
   end
 
-  defp do_block_folding_range({:defmodule, [line: line], [_module, doblock]}) do
-    case doblock do
-      # capture block expression.
-      [do: {:__block__, [], body}] ->
-        # Currently, I didn't set startCharacter and endCharacter because vim-lsp request capabilities
-        # with lineFoldingOnly: true. So we can ignore it for now.
-        [
-          %{
-            "startLine" => line,
-            "endLine" => line + get_last_line(body)
-          }
-        ]
-
-      _ ->
-        []
-    end
+  # {:ok,
+  #  [
+  #    {:identifier, {0, 1, nil}, :defmodule},
+  #    {:alias, {0, 11, nil}, :A},
+  #    {:do, {0, 13, nil}},
+  #    {:eol, {0, 15, 1}},
+  #    {:at_op, {1, 3, nil}, :@},
+  #    {:identifier, {1, 4, nil}, :moduledoc},
+  #    {:bin_string, {1, 14, nil}, ["This is module A"]},
+  #    {:eol, {1, 32, 2}},
+  #    {:identifier, {3, 3, nil}, :def},
+  #    {:paren_identifier, {3, 7, nil}, :hello},
+  #    {:"(", {3, 12, nil}},
+  #    {:")", {3, 13, nil}},
+  #    {:do, {3, 15, nil}},
+  #    {:eol, {3, 17, 1}},
+  #    {:atom, {4, 5, nil}, :world},
+  #    {:eol, {4, 11, 1}},
+  #    {:end, {5, 3, nil}},
+  #    {:eol, {5, 6, 1}},
+  #    {:end, {6, 1, nil}},
+  #    {:eol, {6, 4, 1}}
+  #  ]}
+  defp do_block_folding_range(tokens) when is_list(tokens) do
+    tokens
+    |> Enum.filter(&find_open_close_do_blocks/1)
+    |> pairing_do_end_block()
+    |> Enum.map(fn {{:do, {start_line, _, _}}, {:end, {end_line, _, _}}} ->
+      %{"startLine" => start_line, "endLine" => end_line - 1}
+    end)
   end
 
-  defp do_block_folding_range(_), do: []
+  defp find_open_close_do_blocks({:do, {line, column, _}} = token), do: true
+  defp find_open_close_do_blocks({:end, {line, column, _}} = token), do: true
+  defp find_open_close_do_blocks(_), do: false
 
-  defp get_last_line(exprs) when is_list(exprs),
-    do: get_last_line(Enum.at(exprs, -1))
+  defp pairing_do_end_block([]), do: []
 
-  # Extract last expression from :def or :defp.
-  defp get_last_line({kw, [line: line], [_, block]}) when kw == :def or kw == :defp do
-    case block do
-      [do: {:__block__, _, block}] -> get_last_line(block) + 1
-      # How could we determine between to 2 expressions:
-      #
-      #   def hello, do: :world
-      #
-      # And
-      #
-      #   def hello do
-      #     :world
-      #   end
-      [do: _v] -> line
-    end
+  # TODO: Fix crash in case no match parentheses.
+  # TODO: Fix crash in case we have multiple defmodule in top-level file.
+  defp pairing_do_end_block(tokens) do
+    [open | tail] = tokens
+    [close | tail] = Enum.reverse(tail)
+    [{open, close}] ++ pairing_do_end_block(Enum.reverse(tail))
   end
-
-  defp get_last_line({:case, [line: line], [_, [do: block]]}) do
-    last_line =
-      case Enum.at(block, -1) do
-        {:->, _, [_, expressions]} when is_list(expressions) ->
-          get_last_line(Enum.at(expressions, -1))
-
-        {:->, [line: line], [_, _]} ->
-          line
-
-        _ ->
-          raise "This case should not reach"
-      end
-
-    # Plus 1 for end block
-    last_line + 1
-  end
-
-  defp get_last_line({:raise, [line: line], _}), do: line
 end
--- apps/language_server/test/providers/folding_test.exs
+++ apps/language_server/test/providers/folding_test.exs
@@ -18,7 +18,7 @@ defmodule ElixirLS.LanguageServer.Providers.FoldingTest do
              }
              |> Folding.folding_ranges()
 
-    assert ranges == [%{"endLine" => 3, "startLine" => 0}]
+    assert ranges == [%{"endLine" => 5, "startLine" => 0}, %{"endLine" => 4, "startLine" => 3}]
   end
 
   test "folding complex function" do
@@ -41,7 +41,11 @@ defmodule ElixirLS.LanguageServer.Providers.FoldingTest do
              }
              |> Folding.folding_ranges()
 
-    assert ranges == [%{"endLine" => 10, "startLine" => 0}]
+    assert ranges == [
+             %{"endLine" => 10, "startLine" => 0},
+             %{"endLine" => 9, "startLine" => 3},
+             %{"endLine" => 8, "startLine" => 6}
+           ]
 
     assert {:ok, ranges} =
              %{
@@ -61,9 +65,14 @@ defmodule ElixirLS.LanguageServer.Providers.FoldingTest do
              }
              |> Folding.folding_ranges()
 
-    assert ranges == [%{"endLine" => 9, "startLine" => 0}]
+    assert ranges == [
+             %{"endLine" => 9, "startLine" => 0},
+             %{"endLine" => 8, "startLine" => 3},
+             %{"endLine" => 7, "startLine" => 5}
+           ]
   end
 
+  @tag :f
   # Test self source that can be folding all entire module. It's complex enough
   # use as a test cases. :)
   test "folding.ex" do
@@ -73,5 +82,39 @@ defmodule ElixirLS.LanguageServer.Providers.FoldingTest do
     assert {:ok, ranges} = %{text: f} |> Folding.folding_ranges()
     # 1 for zero-based line and 1 for before last end block.
     assert ranges == [%{"endLine" => lines - 3, "startLine" => 0}]
+  end
+
+  test "multiple defmodule in the top-level of file" do
+    assert {:ok, ranges} =
+             %{
+               text: """
+               defmodule A do
+                 @moduledoc "This is module A"
+                 def hello() do
+                   a = 20
+
+                   case a do
+                     20 -> :ok
+                     _ -> :notok
+                   end
+                 end
+               end
+
+               defmodule B do
+                 @moduledoc "This is module B"
+                 def hello() do
+                   a = 20
+
+                   case a do
+                     20 -> :ok
+                     _ -> :notok
+                   end
+                 end
+               end
+               """
+             }
+             |> Folding.folding_ranges()
+
+    assert ranges == [%{"endLine" => 9, "startLine" => 0}, %{"endLine" => 33, "startLine" => 12}]
   end
 end
