f15a6045f09eae24450f3b8545dba2a9ee8e414c
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2020-08-25 18:21:30 +0700

    Make complex module works
    This changes add a new test by using folding.ex to test itself. But it's
    broken other tests because of AST results that produces by
    Code.string_to_quoted/1.

--- apps/language_server/lib/language_server/providers/folding.ex
+++ apps/language_server/lib/language_server/providers/folding.ex
@@ -1,6 +1,17 @@
 defmodule ElixirLS.LanguageServer.Providers.Folding do
   @moduledoc """
   A textDocument/foldingRange provider implementation.
+
+  The current implementation might not work with the function looks like:
+
+    def hello(), do: :world
+
+    def hello() do
+      :world
+    end
+
+  Because 2 examples above produces the same AST result from
+  Code.string_to_quoted/1.
   """
 
   @doc """
@@ -18,13 +29,14 @@ defmodule ElixirLS.LanguageServer.Providers.Folding do
 
   defp do_block_folding_range({:defmodule, [line: line], [_module, doblock]}) do
     case doblock do
+      # capture block expression.
       [do: {:__block__, [], body}] ->
         # Currently, I didn't set startCharacter and endCharacter because vim-lsp request capabilities
         # with lineFoldingOnly: true. So we can ignore it for now.
         [
           %{
             "startLine" => line,
-            "endLine" => line + get_last_line(body) + 1
+            "endLine" => line + get_last_line(body)
           }
         ]
 
@@ -35,20 +47,38 @@ defmodule ElixirLS.LanguageServer.Providers.Folding do
 
   defp do_block_folding_range(_), do: []
 
-  defp get_last_line(statements) when is_list(statements),
-    do: get_last_line(Enum.at(statements, -1))
+  defp get_last_line(exprs) when is_list(exprs),
+    do: get_last_line(Enum.at(exprs, -1))
 
+  # Extract last expression from :def or :defp.
   defp get_last_line({kw, [line: line], [_, block]}) when kw == :def or kw == :defp do
     case block do
       [do: {:__block__, _, block}] -> get_last_line(block) + 1
-      [do: _v] -> line + 1
+      # How could we determine between to 2 expressions:
+      #
+      #   def hello, do: :world
+      #
+      # And
+      #
+      #   def hello do
+      #     :world
+      #   end
+      [do: _v] -> line
     end
   end
 
   defp get_last_line({:case, [line: line], [_, [do: block]]}) do
     case Enum.at(block, -1) do
-      {:->, [line: line], _} -> line
-      _ -> raise "This case should not reach"
+      {:->, _, [_, expressions]} when is_list(expressions) ->
+        get_last_line(Enum.at(expressions, -1))
+
+      {:->, [line: line], [_, _]} ->
+        line
+
+      _ ->
+        raise "This case should not reach"
     end
   end
+
+  defp get_last_line({:raise, [line: line], _}), do: line
 end
--- apps/language_server/test/providers/folding_test.exs
+++ apps/language_server/test/providers/folding_test.exs
@@ -7,18 +7,18 @@ defmodule ElixirLS.LanguageServer.Providers.FoldingTest do
     assert {:ok, ranges} =
              %{
                text: """
-               defmodule A do # L1
+               defmodule A do
                  @moduledoc "This is module A"
 
-                 def hello() do # L4
+                 def hello() do
                    :world
                  end
-               end # L7
+               end
                """
              }
              |> Folding.folding_ranges()
 
-    assert ranges == [%{"endLine" => 5, "startLine" => 0}]
+    assert ranges == [%{"endLine" => 3, "startLine" => 0}]
   end
 
   test "folding complex function" do
@@ -41,7 +41,7 @@ defmodule ElixirLS.LanguageServer.Providers.FoldingTest do
              }
              |> Folding.folding_ranges()
 
-    assert ranges == [%{"endLine" => 10, "startLine" => 0}]
+    assert ranges == [%{"endLine" => 9, "startLine" => 0}]
 
     assert {:ok, ranges} =
              %{
@@ -61,6 +61,17 @@ defmodule ElixirLS.LanguageServer.Providers.FoldingTest do
              }
              |> Folding.folding_ranges()
 
-    assert ranges == [%{"endLine" => 9, "startLine" => 0}]
+    assert ranges == [%{"endLine" => 8, "startLine" => 0}]
+  end
+
+  # Test self source that can be folding all entire module. It's complex enough
+  # use as a test cases. :)
+  test "folding.ex" do
+    f = File.read!("lib/language_server/providers/folding.ex")
+    lines = String.split(f, "\n") |> Enum.count()
+
+    assert {:ok, ranges} = %{text: f} |> Folding.folding_ranges()
+    # 1 for zero-based line and 1 for before last end block.
+    assert ranges == [%{"endLine" => lines - 3, "startLine" => 0}]
   end
 end
